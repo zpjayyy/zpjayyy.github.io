61da1b58eb4e19fd42ff042c2f84552e9edd469e
zpjayyy <jay.lichzp@gmail.com>
2023-02-22 15:10:06 +0800

    feat: init project

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img')
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else{
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla , ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'hello !!! ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
